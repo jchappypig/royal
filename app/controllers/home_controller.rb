e94ef57bb8cea480af2b5562d1d5865b796c5508
Huanhuan <jchappypig@hotmail.com>
2011-12-15 19:07:29 +1100

    modified home page and added page contact.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @title = "index"
+  end
+
+  def contact
+    @title = "contact"
   end
 
 end
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -6,4 +6,9 @@ class HomeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
 end
